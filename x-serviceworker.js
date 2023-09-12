3b60fe804ab6f31cf61781fcbf8694ec5742aaa2
ingmgni <ingmgni@github.com>
2023-09-12 17:58:24 +0800

    debug

--- x-serviceworker.js
+++ x-serviceworker.js
@@ -11,13 +11,14 @@ self.addEventListener('install', function (event) {
 });
 
 self.addEventListener('message', async function (event) {
+    console.log("serviceworker event:", event)
     clients.matchAll().then(clients => {
         clients.forEach(client => {
             var msg_chan = new MessageChannel();
             client.postMessage(event.data, [msg_chan.port2]);
-            msg_chan.port1.onmessage = function (event) {
-                if (event.data) {
-                    event.ports[0].postMessage(event.data);
+            msg_chan.port1.onmessage = function (_event) {
+                if (_event.data) {
+                    event.ports[0].postMessage(_event.data);
                 }
             };
         })
