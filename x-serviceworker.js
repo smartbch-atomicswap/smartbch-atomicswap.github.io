00de5e6245af8ac8bf3f463e59027f1563c03739
ingmgni <ingmgni@github.com>
2023-09-12 18:26:07 +0800

    1

--- x-serviceworker.js
+++ x-serviceworker.js
@@ -17,6 +17,7 @@ self.addEventListener('message', async function (event) {
             var msg_chan = new MessageChannel();
             client.postMessage(event.data, [msg_chan.port2]);
             msg_chan.port1.onmessage = function (_event) {
+                console.log("_event", _event)
                 if (_event.data) {
                     event.ports[0].postMessage(_event.data);
                 }
